e83b7cc5d71113d00126238ccda079a843e69fea
mukadderbahadir <mukadderbahadir61@gmail.com>
2016-12-09 17:55:20 +0300

    Fixed

--- admin/author.php
+++ admin/author.php
@@ -6,73 +6,57 @@
         <div class="row">
             <form action="" method="post" enctype="multipart/form-data">
                 <div class="twelve columns" align="center">
-                    <div class="six columns" align="center" style="background-color: #D5D3D3;">
-                        <h2>Yazar Ekle</h2>
-                        <?php
-                        $auname= mysql_real_escape_string($_POST['author_user_name']);
-                        $authorpass= mysql_real_escape_string($_POST['author_pass']);
-                        $aname= mysql_real_escape_string($_POST['author_name']);
-                        $asname= mysql_real_escape_string($_POST['author_surname']);
-                        $amail= mysql_real_escape_string($_POST['author_mail']);
-                        $a_add= mysql_real_escape_string($_POST['author_add']);
-                        $apass= md5($authorpass);
+                    <!--YAZAR EKLEME ALANI START-->
+                        <div class="six columns" align="center" style="background-color: #D5D3D3;">
+                            <h2>Yazar Ekle</h2>
+                            <?php
+                            $id = $_POST['id'];
+                            $auname= mysql_real_escape_string($_POST['author_user_name']);
+                            $authorpass= mysql_real_escape_string($_POST['author_pass']);
+                            $aname= mysql_real_escape_string($_POST['author_name']);
+                            $asname= mysql_real_escape_string($_POST['author_surname']);
+                            $amail= mysql_real_escape_string($_POST['author_mail']);
+                            $a_add= mysql_real_escape_string($_POST['author_add']);
+                            $apass= md5($authorpass);
+                            $düzenle = mysql_real_escape_string($_POST['düzenle']);
 
-                        if ($a_add)
-                        {
-                            if ($auname=='' || $authorpass=='' || $aname=='' || $asname=='' || $amail =='')
+
+                            if ($a_add)
                             {
-                                echo "Boş Alan Bıraktınız !";
-                            }else{
-                                $query =mysql_query("insert into writer (author_user_name,a_pass,author_name,author_surname,mail) VALUES ('$auname','$apass','$aname','$asname','$amail')");
-                                 echo "Yazar Eklendi !";
+                                if ($auname=='' || $authorpass=='' || $aname=='' || $asname=='' || $amail =='')
+                                {
+                                    echo "Boş Alan Bıraktınız !";
+                                }else{
+                                    $query =mysql_query("insert into writer (author_user_name,a_pass,author_name,author_surname,mail) VALUES ('$auname','$apass','$aname','$asname','$amail')");
+                                     echo "Yazar Eklendi !";
+                                }
                             }
-                        }
-                        ?>
-                        <form name="frmm" method="post" enctype="multipart/form-data" >
-                            <input  name="author_user_name" class="ten columns" type="text" placeholder="USERNAME">
-                            <input  name="author_pass" class="ten columns" type="password" placeholder="PASSWORD">
-                            <input  name="author_name" class="ten columns" type="text" placeholder="NAME">
-                            <input  name="author_surname" class="ten columns" type="text" placeholder="SURNAME">
-                            <input  name="author_mail" class="ten columns" type="email" placeholder="E-MAİL">
-                            <input type="submit" name="author_add"  value="Yazar Kaydet" class="ten columns  btn btn-primary" style="border-color: black;color:black;margin-top:20px;margin-bottom: 10px">
-                        </form>
-                    </div>
-                   <?php if ($_POST['update'])
-                    {
-                    ?>
-                    <div class="six columns" style="background-color: #D5D3D3">
-
-                        <?php
-
-                        $yad= mysql_real_escape_string($_POST['yad']);
-
-                        $ga_add= mysql_real_escape_string($_POST['guncel']);
+                            ?>
+                            <form name="frmm" method="post" enctype="multipart/form-data" >
+                                <input  name="author_user_name" class="ten columns" type="text" placeholder="USERNAME">
+                                <input  name="author_pass" class="ten columns" type="password" placeholder="PASSWORD">
+                                <input  name="author_name" class="ten columns" type="text" placeholder="NAME">
+                                <input  name="author_surname" class="ten columns" type="text" placeholder="SURNAME">
+                                <input  name="author_mail" class="ten columns" type="email" placeholder="E-MAİL">
+                                <input type="submit" name="author_add"  value="Yazar Kaydet" class="ten columns  btn btn-primary" style="border-color: black;color:black;margin-top:20px;margin-bottom: 10px">
+                            </form>
+                        </div>
+                    <!--YAZAR EKLEME ALANI FINISH-->
 
 
-                        if ($ga_add)
-                        {
-                             $query =mysql_query("update writer set author_user_name='$yad' WHERE author_user_name='d' ");
-                             echo "güncellend";
-                             echo $query;
-                        }
+                   <?php if ($_POST['delete'])
+                    {?>
 
-                        ?>
-
-
-                        <form method="post" name="frmnjd" enctype="multipart/form-data">
-                            <h1> <span>Root</span> Kişisini Güncelle</h1>
-                            <input  name="yad" class="ten columns" type="text" placeholder="yeni kul ad">
-                            <input  name="gpass" class="ten columns" type="password" placeholder="şifre">
-                            <input  name="gname" class="ten columns" type="text" placeholder="ad">
-                            <input  name="gsurname" class="ten columns" type="text" placeholder="soyad">
-                            <input  name="gmail" class="ten columns" type="email" placeholder="mail">
-                            <input type="submit" name="guncel"  value="Yazarı Güncelle" class="ten columns  btn btn-primary" style="border-color: black;color:black;margin-top:20px;margin-bottom: 10px">
-                        </form>
-                    </div>
+                        <div class="six columns" style="background-color: #D5D3D3">
+                            <form method="post" enctype="multipart/form-data">
+                                <input type='text' name='adiniz' placeholder="adiniz">
+                                <input type="submit" name="kaydet"  value="Yazarı Güncelle" class="ten columns  btn btn-primary" >
+                            </form>
+                        </div>
                     <?php
                     }
 
-                    ?>
+                   ?>
 
                     <div class="twelve columns" align="center" style="background-color: darkgray">
                         <h2>Yazarlar</h2>
@@ -81,21 +65,20 @@
                         $say = mysql_fetch_array($query);
                         $sonuc = $say['0'] - 1;
 
-                        for ($number = 0; $number <= $sonuc; $number++)
-                        {
-                            $num = 1 ;
+                        for ($number = 0; $number <= $sonuc; $number++) {
+                            $num = 1;
                             $query = mysql_query("select * from writer order by id ASC limit $number,$num"); //Tüm kayıtları gösterecek sorgu
-                            while ($come = mysql_fetch_assoc($query))
-                            {
+                            while ($come = mysql_fetch_assoc($query)) {
                                 $authors = $come['author_user_name'];
+                                $id = $come ['id'];
+                                echo "<form method='post'><div class='ten columns' align='left'>";
+                                echo "<h2>" . $authors;
+                                echo " <input type='submit' name='delete' value='SİL'>";
+                                echo "<input type='submit' name='düzenle' value='$id'></h2></div></form>";
 
-                                echo "<div class='ten columns' align='left'>";
-                                echo "<h2>".$authors;
-                                echo " <input type='submit' name='delete' value='sil'>";
-                                echo "<input type='submit' name='update' value='güncelle'></h2></div>";
                             }
-                        }
 
+                        }
                         ?>
                     </div>
                 </div>
