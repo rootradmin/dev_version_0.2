37c6a5bdb7812c2e8001d882a70fc8a5bf718be9
rootradmin <mukadderbahadir61@gmail.com>
2017-01-13 09:23:58 +0300

    editing, fixed

--- README.php
+++ README.php
@@ -3,18 +3,10 @@
 
 <!--
 
- 
- **projects bi bak  image opacity ayarı yap %% üzerine url linkini ver
  ** User date topıc=konu iconslarını ayarla
 
 
  ***paylaş butonlarını ekle
-
-    **yazar ekleme ,yönetici izni verilirse yayınlama düzetlme update delete etme ayarları yapılacak
-
-    **yazarları güncellet sil  bunu büyük bir containerde yap !!
-
-    ( if güncelleye basıldıysa yeni bir input alanı açılsın)
 
     **projelere proje silme koy
 
